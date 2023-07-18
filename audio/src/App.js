abda8b39398bc6c45e0d839a997789cebff333d4
sehr <sehr.sehr1112@gmail.com>
2023-07-18 23:36:42 +0500

    create react app

---
+++ audio/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     
+      <h1 className="text-3xl font-bold underline bg-indigo-500">
+      Hello worldf!
+    </h1>
+    <h1>Goodbye world</h1>
+    </div>
+  );
+}
+
+export default App;
